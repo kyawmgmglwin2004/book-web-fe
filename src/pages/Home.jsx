823a619847aef01cc6e5149aa9e079d426e7c359
Kyaw Mg Mg Lwin <kyaw.mg.mg.lwin@team.studioamk.com>
2025-11-10 17:50:02 +0630

    update order

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Card from "../components/Card"
 export default function Home()  {
     return (
         <div className="mt-[0vh]">
+            {/* <div className="w-full">hello</div> */}
             <Card />
         </div>
     )
